f5054a5df0551af84109d0891cc546c8d892605d
externix <extern.fx@gmail.com>
2014-04-13 15:16:56 +0300

    sleep on adding users to queue

--- run.rb
+++ run.rb
@@ -4,6 +4,7 @@ config = YAML.load(File.open('./config/config.yml'))
 input_file = File.new(config['input_filename'], 'r')
 
 input_file.each_line do |line|
+  sleep 0.01
   user_id = line.to_i
   LikerWorker.perform_async(config['application_token'], user_id, config['page_id'])
 end
